c8cf377782b0896173f9fb3a0d963b7c0f6ed1a1
baregl <39744267+baregl@users.noreply.github.com>
2018-09-17 14:18:13 +0200

    Split the syncer run function into parts

--- client/lib/syncer.c
+++ client/lib/syncer.c
@@ -11,36 +11,22 @@
 	(dest)[3] = (value >> 24) & 0xFF;
 
 void add_entry(uint8_t *entry);
+void send_header(char *passwd, char *devname, char *devid, char *ver);
 void send_dir(char *dir);
+void handle_transfer(uint8_t *transfer_req, char *base_dir);
 
 void syncer_run(char *dir, char *devname, char *devid, char *ver, char *passwd)
 {
 	// TODO Handle dir ending without '/'
-	{
-		LOG("Assembling header\n");
-		uint32_t pass = murmur3_32(passwd, strlen(passwd));
-		uint8_t data[header_size] = {'P', 'L', 'Y', 'S',
-					     'Y', 'N', 'C', '1'};
-		strncpy((char *)data + 8, devname, 8);
-		strncpy((char *)data + 16, devid, 8);
-		strncpy((char *)data + 24, ver, 8);
-		data[32] = pass & 0xFF;
-		data[33] = (pass >> 8) & 0xFF;
-		data[34] = (pass >> 16) & 0xFF;
-		data[35] = (pass >> 24) & 0xFF;
-		LOG("Sending header\n");
-		clbk_send(data, sizeof(data));
-	}
+	send_header(passwd, devname, devid, ver);
 	LOG("Sending entries\n");
 	clbk_show_status("Sending entries\n");
 	send_dir(dir);
 	LOG("Sending end of entries\n");
-	{
-		uint8_t entry[entry_size] = {};
-		// Iterate over directory
-		entry[0] = 'e';
-		add_entry(entry);
-	}
+	uint8_t entry[entry_size] = {};
+	// Iterate over directory
+	entry[0] = 'e';
+	add_entry(entry);
 	uint8_t transfer_req[transfer_req_size + 1];
 	// Just to make sure the 0th byte isn't 'e'
 	transfer_req[0] = 0;
@@ -53,49 +39,7 @@ void syncer_run(char *dir, char *devname, char *devid, char *ver, char *passwd)
 					"connection interrupted");
 		}
 		LOG("Received request\n");
-		if (transfer_req[0] == 'f' || transfer_req[0] == 'h') {
-			if (strncmp((char *)transfer_req + 1, dir,
-				    strlen(dir)) != 0)
-				clbk_show_error("File not within base dir");
-			// Just so this isn't stack allocated
-			static uint8_t transfer_buffer[transfer_size];
-			uint32_t size =
-			    clbk_file_size((char *)transfer_req + 1);
-			LOG("File size is %li\n", size);
-			if (transfer_req[0] == 'f') {
-				bytiffy_uint32(transfer_buffer, size);
-				clbk_send(transfer_buffer, 4);
-				LOG("Sent file size\n");
-			}
-			if (clbk_open((char *)transfer_req + 1) == -1) {
-				clbk_show_status("Couldn't open ");
-				clbk_show_status((char *)transfer_req + 1);
-				clbk_show_error("");
-			}
-			uint32_t read;
-			uint32_t h = 0;
-			if (transfer_req[0] == 'h')
-				clbk_show_status("Hashing ");
-			else
-				clbk_show_status("Sending ");
-			clbk_show_status((char *)transfer_req + 1);
-			clbk_show_status("\n");
-			while ((read = clbk_read(transfer_buffer,
-						 sizeof(transfer_buffer))) ==
-			       sizeof(transfer_buffer)) {
-				h = murmur3_32_step(h, transfer_buffer, read);
-				if (transfer_req[0] == 'f')
-					clbk_send(transfer_buffer, read);
-			}
-			h = murmur3_32_finalize(h, transfer_buffer, read, size);
-			if (transfer_req[0] == 'f') {
-				clbk_send(transfer_buffer, read);
-				LOG("Sent file %s\n", (char *)transfer_req + 1);
-			}
-			bytiffy_uint32(transfer_buffer, h);
-			clbk_send(transfer_buffer, 4);
-			LOG("Sent checksum %lx\n", h);
-		}
+		handle_transfer(transfer_req, dir);
 	} while (transfer_req[0] != 'e');
 	clbk_show_status("Done\n");
 }
@@ -111,6 +55,21 @@ void add_entry(uint8_t *entry)
 	}
 }
 
+void send_header(char *passwd, char *devname, char *devid, char *ver)
+{
+	LOG("Assembling header\n");
+	uint32_t pass = murmur3_32(passwd, strlen(passwd));
+	uint8_t data[header_size] = {'P', 'L', 'Y', 'S', 'Y', 'N', 'C', '1'};
+	strncpy((char *)data + 8, devname, 8);
+	strncpy((char *)data + 16, devid, 8);
+	strncpy((char *)data + 24, ver, 8);
+	data[32] = pass & 0xFF;
+	data[33] = (pass >> 8) & 0xFF;
+	data[34] = (pass >> 16) & 0xFF;
+	data[35] = (pass >> 24) & 0xFF;
+	LOG("Sending header\n");
+	clbk_send(data, sizeof(data));
+}
 void send_dir(char *dir)
 {
 	LOG("Opening %s\n", dir);
@@ -161,4 +120,50 @@ void send_dir(char *dir)
 		}
 	}
 	clbk_close_dir(dird);
+}
+
+void handle_transfer(uint8_t *transfer_req, char *base_dir)
+{
+	if (transfer_req[0] == 'f' || transfer_req[0] == 'h') {
+		if (strncmp((char *)transfer_req + 1, base_dir,
+			    strlen(base_dir)) != 0)
+			clbk_show_error("File not within base dir");
+		// Just so this isn't stack allocated
+		static uint8_t transfer_buffer[transfer_size];
+		uint32_t size = clbk_file_size((char *)transfer_req + 1);
+		LOG("File size is %li\n", size);
+		if (transfer_req[0] == 'f') {
+			bytiffy_uint32(transfer_buffer, size);
+			clbk_send(transfer_buffer, 4);
+			LOG("Sent file size\n");
+		}
+		if (clbk_open((char *)transfer_req + 1) == -1) {
+			clbk_show_status("Couldn't open ");
+			clbk_show_status((char *)transfer_req + 1);
+			clbk_show_error("");
+		}
+		uint32_t read;
+		uint32_t h = 0;
+		if (transfer_req[0] == 'h')
+			clbk_show_status("Hashing ");
+		else
+			clbk_show_status("Sending ");
+		clbk_show_status((char *)transfer_req + 1);
+		clbk_show_status("\n");
+		while ((read = clbk_read(transfer_buffer,
+					 sizeof(transfer_buffer))) ==
+		       sizeof(transfer_buffer)) {
+			h = murmur3_32_step(h, transfer_buffer, read);
+			if (transfer_req[0] == 'f')
+				clbk_send(transfer_buffer, read);
+		}
+		h = murmur3_32_finalize(h, transfer_buffer, read, size);
+		if (transfer_req[0] == 'f') {
+			clbk_send(transfer_buffer, read);
+			LOG("Sent file %s\n", (char *)transfer_req + 1);
+		}
+		bytiffy_uint32(transfer_buffer, h);
+		clbk_send(transfer_buffer, 4);
+		LOG("Sent checksum %lx\n", h);
+	}
 }
